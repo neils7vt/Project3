448975992f122c62e1b88d1cf36ead078edc637f
Neil Sankineni <neils7@vt.edu>
2020-12-04 17:10:26 -0500

    Implemented function to list all files in directory and subdirectories. Issues with loop present

--- Project3.py
+++ Project3.py
@@ -1,10 +1,23 @@
 #! /bin/usr/python3
 import os
 
+def get_files(dir):
+    #Function to get the list of all files in a given directory and its subdirectories
+    for root, directories, files in os.walk(dir):
+        for filename in files:
+
+            file_path = os.path.join(root, filename)
+            print (file_path)
+
+
+
 user_input = input("Do you want to play the game?")
 
 if user_input in ('YES', 'Yes', 'yes', 'yeah', 'Yeah', 'y', 'Y'):
     user_option = 'True'
+
+#This returns the current working directory
+currdir = os.getcwd()
 
 while user_option == 'True':
     print("Please choose one of the options listed below: ")
@@ -13,6 +26,25 @@ while user_option == 'True':
 
 
     user_selection = input("Please select an option: ")
+    
+    if user_selection == '1': 
+
+        testdir = input("Please enter the full path of the directory you want to list the files of: ")
+        if testdir == '':
+            testdir = currdir
+            print("You did not enter any value, using the current directory to list files")
+
+        get_files(testdir)
+
+    else:
+         print("Not a valid selection")
+    user_input = input("Do you want to continue?")
+    if user_input in ('YES', 'Yes', 'yes', 'yeah', 'Yeah', 'y', 'Y'):
+        user_option = 'True'
+    else:
+        print("Thank you for playing the game")
+
+        
 
 
 
