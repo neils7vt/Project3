d5fd7d1c1a4f412abe41fc90f429db5a5c4159e7
Neil Sankineni <neils7@vt.edu>
2020-12-04 16:41:20 -0500

    Added a list of menu options for user to select

--- Project3.py
+++ Project3.py
@@ -1,1 +1,18 @@
 #! /bin/usr/python3
+import os
+
+user_input = input("Do you want to play the game?")
+
+if user_input in ('YES', 'Yes', 'yes', 'yeah', 'Yeah', 'y', 'Y'):
+    user_option = 'True'
+
+while user_option == 'True':
+    print("Please choose one of the options listed below: ")
+    print("1. List the files present in a given directory and all subdirectories")
+    print("2. Find the total number of files in a given directory and all subdirectories")
+
+
+    user_selection = input("Please select an option: ")
+
+
+
